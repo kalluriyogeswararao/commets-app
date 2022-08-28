ae80a45daebaaab898b94049a41137f94a19dcbb
kalluriyogeswararao <kalluriyogesh9@gmail.com>
2022-08-28 08:14:04 +0000

    remaining code

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,17 @@
-// Write your code here
+import './index.css'
+
+const CommentItem = props => {
+  const {commentDetails} = props
+  const {name, comment} = commentDetails
+
+  return (
+    <li className="comment-data">
+      <div className="details">
+        <p className="profile">{name[0].toUpperCase()}</p>
+        <h1 className="profile-name">{name}</h1>
+      </div>
+      <p className="comment">{comment}</p>
+    </li>
+  )
+}
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,4 +1,6 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import CommentItem from '../CommentItem'
 
 import './index.css'
 
@@ -13,34 +15,86 @@ const initialContainerBackgroundClassNames = [
 ]
 
 class Comments extends Component {
+  state = {commentList: [], name: '', comment: ''}
+
+  onChangeName = event => {
+    this.setState({name: event.target.value})
+  }
+
+  onChangeComment = event => {
+    this.setState({comment: event.target.value})
+  }
+
+  addComments = event => {
+    event.preventDefault()
+    const {name, comment} = this.state
+    const addComment = {
+      id: uuidv4(),
+      name,
+      comment,
+    }
+    this.setState(prevState => ({
+      commentList: [...prevState.commentList, addComment],
+      name: '',
+      comment: '',
+    }))
+  }
+
   render() {
+    const {commentList, name, comment} = this.state
+    const count = commentList.length
     return (
       <div className="bg-container">
-        <div className="comment-container">
-          <div>
-            <h1 className="main-heading">Comments</h1>
-            <p className="para">Say something about 4.O Technologies</p>
+        <div className="top">
+          <div className="comment-container">
+            <div>
+              <h1 className="main-heading">Comments</h1>
+              <p className="para">Say something about 4.O Technologies</p>
+            </div>
+            <form className="form-container" onSubmit={this.addComments}>
+              <input
+                type="text"
+                className="input"
+                placeholder="Your name"
+                onChange={this.onChangeName}
+                value={name}
+              />
+              <textarea
+                rows="15"
+                cols="40"
+                className="textarea"
+                placeholder="Your Comment"
+                onChange={this.onChangeComment}
+                value={comment}
+              />
+              <button type="submit" className="button">
+                Add Comment
+              </button>
+            </form>
           </div>
-          <form className="form-container">
-            <input type="text" className="input" placeholder="Your name" />
-            <textarea
-              rows="15"
-              cols="40"
-              className="textarea"
-              placeholder="Your Comment"
-            />
-            <button type="submit" className="button">
-              Add Comment
-            </button>
-          </form>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+            alt="comments"
+            className="image"
+          />
         </div>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
-          alt="comments"
-          className="image"
-        />
 
         <hr />
+        <div className="count-container">
+          <p className="comment-count">{count}</p>
+          <p>comments</p>
+        </div>
+        <ul className="all-comments">
+          {commentList.map(eachComment => (
+            <CommentItem
+              commentDetails={eachComment}
+              key={eachComment.id}
+              initialContainerBackgroundClassNames={
+                initialContainerBackgroundClassNames
+              }
+            />
+          ))}
+        </ul>
       </div>
     )
   }
